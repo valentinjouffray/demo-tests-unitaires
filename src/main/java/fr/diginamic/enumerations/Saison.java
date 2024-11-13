f4935df7e631d4ec1badd99fd3e2ac58d339f6a6
Valentin JOUFFRAY <valentin.jouffray@gmail.com>
2024-11-13 10:54:34 +0100

    exo1 fini

--- src/main/java/fr/diginamic/enumerations/Saison.java
+++ src/main/java/fr/diginamic/enumerations/Saison.java
@@ -1,70 +1,78 @@
 package fr.diginamic.enumerations;
+
+import java.text.Collator;
+import java.util.Locale;
+
 /**
  * Représente une saison et fournit les 4 instances de saisons.
- * 
- * @author DIGINAMIC
  *
+ * @author DIGINAMIC
  */
 public enum Saison {
 
-	/** Les 4 instances de Saison */
-	PRINTEMPS("Printemps", 1), ETE("Eté", 2), AUTOMNE("Automne", 3), HIVER("Hiver", 4);
+    /**
+     * Les 4 instances de Saison
+     */
+    PRINTEMPS("Printemps", 1), ETE("Eté", 2), AUTOMNE("Automne", 3), HIVER("Hiver", 4);
 
-	/** libelle */
-	private String libelle;
-	/** ordre */
-	private int ordre;
+    /**
+     * libelle
+     */
+    private final String libelle;
+    /**
+     * ordre
+     */
+    private final int ordre;
 
-	/**
-	 * Constructeur
-	 * 
-	 * @param libelle libellé
-	 * @param ordre   ordre
-	 */
-	private Saison(String libelle, int ordre) {
-		this.libelle = libelle;
-		this.ordre = ordre;
-	}
+    /**
+     * Constructeur
+     *
+     * @param libelle libellé
+     * @param ordre   ordre
+     */
+    Saison(String libelle, int ordre) {
+        this.libelle = libelle;
+        this.ordre = ordre;
+    }
 
-	/**
-	 * Permet de rechercher une Saison à partir de son libellé
-	 * 
-	 * @param libelle libellé
-	 * @return {@link Saison}
-	 */
-	public static Saison valueOfLibelle(String libelle) {
-		Saison[] saisons = Saison.values();
-		for (Saison saison : saisons) {
-			if (libelle.equals(saison.getLibelle())) {
-				return saison;
-			}
-			else {
-				return null;
-			}
-		}
-		return null;
-	}
+    /**
+     * Permet de rechercher une Saison à partir de son libellé
+     *
+     * @param libelle libellé
+     * @return {@link Saison}
+     */
+    public static Saison valueOfLibelle(String libelle) {
+        Collator collator = Collator.getInstance(Locale.FRANCE);
+        collator.setStrength(Collator.PRIMARY);
+        Saison[] saisons = Saison.values();
+        for (Saison saison : saisons) {
+            if (collator.equals(libelle, saison.getLibelle())) {
+                return saison;
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public String toString() {
-		return ordre + ". " + libelle;
-	}
+    @Override
+    public String toString() {
+        return ordre + ". " + libelle;
+    }
 
-	/**
-	 * Getter
-	 * 
-	 * @return the libelle
-	 */
-	public String getLibelle() {
-		return libelle;
-	}
+    /**
+     * Getter
+     *
+     * @return the libelle
+     */
+    public String getLibelle() {
+        return libelle;
+    }
 
-	/**
-	 * Getter
-	 * 
-	 * @return the ordre
-	 */
-	public int getOrdre() {
-		return ordre;
-	}
+    /**
+     * Getter
+     *
+     * @return the ordre
+     */
+    public int getOrdre() {
+        return ordre;
+    }
 }
---
+++ src/test/java/fr/diginamic/enumerations/SaisonTest.java
@@ -0,0 +1,50 @@
+package fr.diginamic.enumerations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SaisonTest {
+
+    @Test
+    public void valueOfLibelle() {
+        final String libelle = "Printemps";
+        final Saison saison = Saison.valueOfLibelle(libelle);
+        assertEquals(saison, Saison.PRINTEMPS);
+
+        final String libelle2 = "printemps";
+        final Saison saison2 = Saison.valueOfLibelle(libelle2);
+        assertEquals(saison2, Saison.PRINTEMPS);
+
+        final String libelle3 = "Eté";
+        final Saison saison3 = Saison.valueOfLibelle(libelle3);
+        assertEquals(saison3, Saison.ETE);
+
+        final String libelle4 = "été";
+        final Saison saison4 = Saison.valueOfLibelle(libelle4);
+        assertEquals(saison4, Saison.ETE);
+
+        final String libelle5 = "Automne";
+        final Saison saison5 = Saison.valueOfLibelle(libelle5);
+        assertEquals(saison5, Saison.AUTOMNE);
+
+        final String libelle6 = "automne";
+        final Saison saison6 = Saison.valueOfLibelle(libelle6);
+        assertEquals(saison6, Saison.AUTOMNE);
+    }
+
+    @Test
+    public void getLibelle() {
+        final Saison saison = Saison.PRINTEMPS;
+        assertEquals(saison.getLibelle(), "Printemps");
+
+        final Saison saison2 = Saison.ETE;
+        assertEquals(saison2.getLibelle(), "Eté");
+
+        final Saison saison3 = Saison.AUTOMNE;
+        assertEquals(saison3.getLibelle(), "Automne");
+
+        final Saison saison4 = Saison.HIVER;
+        assertEquals(saison4.getLibelle(), "Hiver");
+    }
+}
